996c68c7fc82f4d8caa2376fbb3ef364c55b8a45
Prishita Kadam <55227184+prishitakadam@users.noreply.github.com>
2021-04-16 00:40:16 +0530

    Update 1822. Sign of the Product of an Array.cpp

--- Problems/1822. Sign of the Product of an Array.cpp
+++ Problems/1822. Sign of the Product of an Array.cpp
@@ -1,3 +1,43 @@
+//0ms Solution
+
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int start = 0, end = nums.size()-1, count = 0, ans = -1;
+        
+        while(end>=start){
+            if(nums[end] == 0 || nums[start] == 0){
+                ans = 0;
+                count = -1;
+                break;
+            }
+            else{
+                if(end == start){
+                    if(nums[end] < 0)
+                        count += 1;
+                }
+                else{
+                    if(nums[end] < 0)
+                        count += 1;
+                    if(nums[start] < 0)
+                        count += 1;
+                }
+            }
+            
+            start += 1;
+            end -= 1;
+        }
+        
+        if(count%2 == 0 && count != -1)
+            ans = 1;
+
+
+        return ans;
+    }
+};
+
+//Alternate Solution by using Sort
+
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
