b1339eeb8d509e1288c9de23b74f7428de746674
Prishita Kadam <55227184+prishitakadam@users.noreply.github.com>
2021-04-23 12:45:50 +0530

    Create 1502. Can Make Arithmetic Progression From Sequence.cpp

---
+++ Problems/1502. Can Make Arithmetic Progression From Sequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        if(arr.size() == 2)
+            return true;
+        else{
+            int diff = arr[1] - arr[0];
+            for(int i=1; i<arr.size()-1; i++){
+                if(arr[i+1] - arr[i] != diff && arr[i+1] - arr[i] != -diff)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
