c370b5e8dd9ca3fb6a985200c19ba666b0a793da
Prishita Kadam <55227184+prishitakadam@users.noreply.github.com>
2021-05-01 16:17:20 +0530

    Update 575. Distribute Candies.cpp

--- Problems/575. Distribute Candies.cpp
+++ Problems/575. Distribute Candies.cpp
@@ -1,3 +1,25 @@
+//Using Sort, O(nlogn), Beats 96.20%
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        int unique = 1;
+        sort(candyType.begin(), candyType.end());
+        int* p = candyType.data();
+        
+        for(int i=0; i<candyType.size()-1; i++){
+            if(p[i] != p[i+1])
+                unique++;
+        }
+        
+        if(unique < candyType.size()/2)
+            return unique;
+        else
+            return candyType.size()/2;
+    }
+};
+
+
+//Using Set
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
