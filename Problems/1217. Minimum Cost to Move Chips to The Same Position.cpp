e1f5b97c2902fc4f351fbe10b5b60bb71d126c48
Prishita Kadam <55227184+prishitakadam@users.noreply.github.com>
2021-05-20 23:17:29 +0530

    Update 1217. Minimum Cost to Move Chips to The Same Position.cpp

--- Problems/1217. Minimum Cost to Move Chips to The Same Position.cpp
+++ Problems/1217. Minimum Cost to Move Chips to The Same Position.cpp
@@ -3,12 +3,7 @@ public:
     int minCostToMoveChips(vector<int>& position) {
         int even = 0, odd = 0;
         
-        for(auto const& x: position){
-            if(x % 2 ==0)
-                even++;
-            else
-                odd++;
-        }
+        for(auto const& x: position) (x % 2 == 0)? even++:odd++;
         
         return min(even, odd);
     }
